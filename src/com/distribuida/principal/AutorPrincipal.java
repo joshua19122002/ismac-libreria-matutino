38643e540bf899ead7a12de072e91a62cba50d7e
2200385389 <joshua@LAB-SW3-04.ALUMNOS.LOCAL>
2024-11-15 11:25:42 -0500

    creacion de AutorPrincipal

---
+++ src/com/distribuida/principal/AutorPrincipal.java
@@ -0,0 +1,11 @@
+package com.distribuida.principal;
+
+public class AutorPrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// clase principal autor para gestionar datos
+
+	}
+
+}
